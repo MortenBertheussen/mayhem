5f5fd5761d685c2164815b7d5fea88f795155962
Mellet Solbakk <mellet89@gmail.com>
2016-03-15 16:52:51 +0100

    Added moving object file and minor changed to initial setup

---
+++ Movingobjects.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+class Movingobject:
+	def __init__(self):
+		self.gravity = 5
+
+	def rotate(self):
+		pass
+
+class Rocket(Movingobject): 
+	"""The class for rocket, broombroom"""
+	def __initi__(self):
+		super().__init__(self)
--- mayhem.py
+++ mayhem.py
@@ -6,32 +6,28 @@ import pygame
 import random
 import cProfile
 from Vector2D import *
+from Movingobjects import *
 
-
+## GAME CONSTANTS ##
 SCREEN_X = 1920
 SCREEN_Y = 1080
 SCREEN = (SCREEN_X, SCREEN_Y)
-
-
-
-BACKGROUND_FNAME = "sprites/arcadebackground.jpg"
-background = pygame.image.load(BACKGROUND_FNAME)
-background = pygame.transform.scale(background, (SCREEN_X, SCREEN_Y))
-
+FPS = 60
 RED = (204,0,0)
 BLUE = (0,50,255)
 YELLOW = (230,230,30)
 WHITE = (255,255,255)
 BLACK = (0,0,0)
 
-FPS = 60
+BACKGROUND_FNAME = "sprites/arcadebackground.jpg"
+background = pygame.image.load(BACKGROUND_FNAME)
+background = pygame.transform.scale(background, (SCREEN_X, SCREEN_Y))
 
 class Engine:
 	"""This is the engine class"""
 	def __init__(self):
 		self.rockets = []
 		self.rocketshot = []
-
 
 	def eventhandler(self):
 		"""The eventhandler"""
@@ -42,30 +38,18 @@ class Engine:
 			if event.type == pygame.KEYDOWN:
 				if event.key == pygame.K_q:
 					exit()
+
 	def logic(self):
 		self.eventhandler()
 
 	def wallcollide(self):
 		pass
-		
-class Movingobject:
-	def __init__(self):
-		self.gravity = 5
-
-	def rotate(self):
-		pass
-
-class Rocket(Movingobject): 
-	"""The class for rocket, broombroom"""
-	def __initi__(self):
-		super().__init__(self)
-
 
 def main():
 	pygame.init()
 	pygame.display.set_caption("Mayhem")
-	screen = pygame.display.set_mode((SCREEN),pygame.FULLSCREEN)	#FULLSCREEN
-	#screen = pygame.display.set_mode((SCREEN), 0, 32 )				#WINDOWED
+	#screen = pygame.display.set_mode((SCREEN),pygame.FULLSCREEN)	#FULLSCREEN
+	screen = pygame.display.set_mode((SCREEN), 0, 32 )				#WINDOWED
 
 	clock = pygame.time.Clock()
 	time = clock.tick(FPS)
