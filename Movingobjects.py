3c6036b037bda91b8f7b8a627f472f30c127b2e7
Mellet Solbakk <mellet89@gmail.com>
2016-03-15 17:27:19 +0100

    Added some values for Rocket and started implementing draw and move methods

--- Movingobjects.py
+++ Movingobjects.py
@@ -2,12 +2,25 @@
 
 class Movingobject:
 	def __init__(self):
-		self.gravity = 5
+		self.gravity = Vector2D(0,9)
 
 	def rotate(self):
 		pass
 
 class Rocket(Movingobject): 
 	"""The class for rocket, broombroom"""
-	def __initi__(self):
+	def __init__(self):
 		super().__init__(self)
+		self.pos = Vector2D(0,0)
+		self.speed = Vector2D(0,0)
+		self.engineOn = False
+
+	def draw(self):
+		"""Draw the rocket in its current possition."""
+		pygame.draw.rect(screen, (150,0,0), (self.possition.x, self.pos.y, self.witdh, self.height), 0)
+
+	def move(self):
+		if engineOn:
+			self.pos += self.speed + self.gravity
+		else:
+			self.pos += self.gravity
