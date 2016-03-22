5be5586f3b2b6d47ea1b8b928225d26dddcbea90
Mellet Solbakk <mellet89@gmail.com>
2016-03-23 00:45:47 +0100

    Commenting and angle fix 1 Added some commenting for clarity 2 Confine angle between 0 and 360 degrees. Makes it easier to find out what direction to rotate it when gravity is the only force affecting it

--- Movingobjects.py
+++ Movingobjects.py
@@ -53,31 +53,32 @@ class Rocket(Movingobject):
 	def logic(self, screen):
 		"""Logic method of rocket"""
 		self.speed_limit()
+		self.angle_fix()
 		self.move()
 		self.screen_wrap()
 		self.fule()
 		#Hitbox for bugtesting
 		#pygame.draw.rect(screen,RED,(self.rect.x,self.rect.y,self.rect.height,self.rect.width))
 
-	def move(self):
-		"""move method of rocket"""
+	def angle_fix(self):
+		"""Keeps the angle between 0 and 360 degrees. We dont want negative angles."""
+		if self.angle > 360:
+			self.angle = 0
+		if self.angle < 0:
+			self.angle = 360
 
+	def move(self):
+		"""Move method of rocket"""
 		if self.turnLeft:
 			self.angle += 4
 		if self.turnRight:
 			self.angle -= 4
-
-		#Reset angle if it goes over 360 back to 0
-		if self.angle > 360:
-			self.angle = 0
-		if self.angle < -360:
-			self.angle = 0
 		
 		new_speed = self.rotate()
 
 		#Movement if engine is on
 		if self.engineOn and self.fuel>0:
-			self.update_sprite("sprites/ship_engine_on.png")
+			self.update_sprite("sprites/ship_engine_on.png")	#Update sprite to engine on image
 			self.direction *= 1.5
 			if self.refuel is False:
 				self.pos += new_speed #+ self.gravity/2
@@ -88,23 +89,24 @@ class Rocket(Movingobject):
 			#set fuel not to go under 0
 			if self.fuel <= 0:
 				self.fuel = 0
+		#Movement if engine is off
 		else:
-			self.update_sprite("sprites/ship.png")
+			self.update_sprite("sprites/ship.png")				#Update sprite to engine off image
+			#Movement when ship still has big acceleration and not refuling
 			if self.direction.magnitude() > 0.5 and self.refuel is False:
 				self.direction /= 1.04
 				self.pos += new_speed + self.gravity
+			#Movement when ship has very low acceleration or it is refuling
 			else:
-				self.pos += self.gravity
-			self.rect.center = (self.pos.x, self.pos.y)
+				self.pos += self.gravity	#Only gravity works on the rockets position
 
-			#Rotate slowly back to 0 deg if engine is off
-			if 1 < self.angle < -1:
-				self.angle = 0
-			else:
-				if self.angle < 0:
-					self.angle += 1
-				if self.angle > 0:
-					self.angle -= 1
+			self.rect.center = (self.pos.x, self.pos.y) #Update the rockets center position
+
+			#Rotate slowly back to 0 deg if engine is off (Looks like gravity is pulling the rockets center of mass downwards)
+			if self.angle != 0 and self.angle > 180:	#If the angle is bigger than 180 deg we increase it towards 360(same as 0deg)
+				self.angle += 1
+			if self.angle !=0 and self.angle < 180:		#If the angle is smaller than 180 deg we decrease it towards 0
+				self.angle -= 1
 
 	def speed_limit(self):
 		"""Speed_limit method of rocket"""
@@ -119,7 +121,7 @@ class Rocket(Movingobject):
 		"""Screen_wrap method of rocket"""
 		#Left
 		if self.pos.x <= 0:
-			self.pos.x = SCREEN_X - 1
+			self.pos.x = SCREEN_X
 		#Right
 		if self.pos.x >= SCREEN_X:
 			self.pos.x = 1
