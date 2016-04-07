64cb4b9a8ce1f2cbd7aa46a56bd152c531ed26c1
Mellet Solbakk <mellet89@gmail.com>
2016-04-08 00:08:29 +0200

    Docstring ang minor cleanup

--- movingobject.py
+++ movingobject.py
@@ -7,28 +7,30 @@ import math
 RESPAWN_TIMER = pygame.USEREVENT + 2
 
 class Movingobject(pygame.sprite.Sprite):
-	"""A class for movingobjects"""
+	"""
+	Superclass for all moving objects.
+	"""
 	def __init__(self):
 		super().__init__()
-		self.pos = Vector2D(SCREEN_X/2 - 22, SCREEN_Y/2 - 22)
-		self.speed = Vector2D(0,-1)
+		self.pos = Vector2D(0, 0)
+		self.speed = Vector2D(0,-1)		#Initial direction pointing upwards
 		self.angle = 0
-		self.maxspeed = 5
+		self.maxspeed = 5				#Default maxspeed if not redefined by child
 
 	def screen_wrap(self):
-		"""Make moving objects wrap around if they go out of the screen"""
+		"""
+		Makes the object wrap if it goes outside the screen border.
+		"""
 		if self.pos.x <= 0: 		self.pos.x = SCREEN_X - 1	#Left
 		if self.pos.x >= SCREEN_X:	self.pos.x = 1				#Right
 		if self.pos.y >= SCREEN_Y:	self.pos.y = 1				#Bottom
 		if self.pos.y <= 0:			self.pos.y = SCREEN_Y - 1 	#Top
 
-	def rotate(self):
-		"""The math behind finding the new direction with an angle"""
-		x = self.speed.x * math.cos(math.radians(-self.angle)) - self.speed.y * math.sin(math.radians(-self.angle))
-		y = self.speed.x * math.sin(math.radians(-self.angle)) + self.speed.y * math.cos(math.radians(-self.angle))
-		return new_speed
-
 	def rotate_point(self, point, center, angle):
+		"""
+		Rotate a point with a set angle around a center point. Returns a vector for the new point.
+ 		Usage: newpoint = rotate_point( (Vector2D)oldpoint, (Vector2D)centerpoint, (int)angle)
+		"""
 		ang = math.radians(-angle)
 		x1 = point.x - center.x
 		y1 = point.y - center.y
@@ -40,25 +42,40 @@ class Movingobject(pygame.sprite.Sprite):
 		return Vector2D(pointx,pointy)
 
 	def new_sprite(self, rect):
-		"""Fetches a new sprite from the spritesheet and keep its rotation"""
+		"""
+		Fetches a new sprite from the spritesheet and rotates it to the angle stored by the object.
+		Usage: image = new_sprite( (x,y,w,h) )
+		"""
 		oldrect = self.rect
 		self.image = self.spritesheet.get_image(rect)
 		self.image = pygame.transform.rotate(self.image, self.angle)
-		self.rect = self.image.get_rect(center=oldrect.center)
+		self.rect = self.image.get_rect(center=oldrect.center)			#Realign the image to be at the center where the old image was
 
 	def rotate_left(self):
-		x = self.speed.x * math.cos(math.radians(-4)) - self.speed.y * math.sin(math.radians(-4))
-		y = self.speed.x * math.sin(math.radians(-4)) + self.speed.y * math.cos(math.radians(-4))
+		"""
+		Rotate the speed vector left 4 degrees
+		"""
+		rad = math.radians(-4)
+		x = self.speed.x * math.cos(rad) - self.speed.y * math.sin(rad)
+		y = self.speed.x * math.sin(rad) + self.speed.y * math.cos(rad)
 		self.speed = Vector2D(x, y)
 		self.calc_angle()
 
 	def rotate_right(self):
-		x = self.speed.x * math.cos(math.radians(4)) - self.speed.y * math.sin(math.radians(4))
-		y = self.speed.x * math.sin(math.radians(4)) + self.speed.y * math.cos(math.radians(4))
+		"""
+		Rotate the speed vector right 4 degrees
+		"""
+		rad = math.radians(4)
+		x = self.speed.x * math.cos(rad) - self.speed.y * math.sin(rad)
+		y = self.speed.x * math.sin(rad) + self.speed.y * math.cos(rad)
 		self.speed = Vector2D(x, y)
 		self.calc_angle()
 
 	def calc_angle(self):
+		"""
+		Calculates the angle for the speed vector if it has been modified slightly by gravity.
+		If this is not done the image angle and the speed vector angle will be slightly off.
+		"""
 		baseline = Vector2D(0,-1) #Vector pointing upwards
 		dot = (self.speed.x * baseline.x) + (self.speed.y * baseline.y)
 		det = (self.speed.x * baseline.y) - (self.speed.y * baseline.x)
@@ -66,6 +83,8 @@ class Movingobject(pygame.sprite.Sprite):
 		self.angle = math.degrees(angle)
 
 	def speed_limit(self):
-		"""Speed_limit method of rocket"""
+		"""
+		Universal speed limit for moving objects.
+		"""
 		if self.speed.magnitude() > self.maxspeed:
 			self.speed = self.speed.normalized() * self.maxspeed
